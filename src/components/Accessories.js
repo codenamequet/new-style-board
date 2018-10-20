e6856454438541469a8ab392f7bb11086538fea8
Marquet Reid <you@example.com>
2018-10-20 17:21:10 -0400

    added axios call for accessories and working on accessories page

--- src/components/Accessories.js
+++ src/components/Accessories.js
@@ -2,8 +2,15 @@ import React, {Component} from 'react'
 
 class Accessories extends Component {
   render() {
+    const accImages = this.props.accessories[0]
+    // console.log(`props: ${accImages}`)
+    console.log(this.props.accessories[0])
+    // const accessory = accImages.map(img => console.log(img))
     return (
-      <p>This is Accessories</p>
+      <div>
+      <p>These are the accessories</p>
+      {/* <img src={accessory} alt={this.props.state.accessories.name} /> // whatever dot notation returns the images */}
+      </div>
     )
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Nav from './Nav'
 import Accessories from './Accessories'
 import Shirts from './Shirts'
 import Pants from './Pants'
 import Shoes from './Shoes'
+import {ACCESSORIES_URL} from '../constants.js'
 
 class App extends Component {
+  state = {
+    accessories: {},
+    shirts: {},
+    pants: {},
+    shoes: {}
+  }
+
+  componentDidMount() {
+    axios
+    .get(`${ACCESSORIES_URL}`)
+    .then(res => this.setState({accessories: res.data.accessories}))
+    .catch (err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        <Accessories />
-        <Shirts />
-        <Pants />
-        <Shoes />
+        <Nav />
+        <Accessories accessories={this.state.accessories} />
+        <Shirts shirts={this.state.shirts} />
+        <Pants pants={this.state.pants} />
+        <Shoes shoes={this.state.shoes} />
       </div>
     );
   }
