34bb9eedb090e98bc6055eea587f098130c12ffc
Marquet Reid <you@example.com>
2018-10-21 00:13:17 -0400

    displayed images to page working on dynamic images

--- src/components/Accessories.js
+++ src/components/Accessories.js
@@ -2,14 +2,27 @@ import React, {Component} from 'react'
 
 class Accessories extends Component {
   render() {
-    const accImages = this.props.accessories[0]
-    // console.log(`props: ${accImages}`)
-    console.log(this.props.accessories[0])
-    // const accessory = accImages.map(img => console.log(img))
+    const accessory = this.props.accessories
+    // for (let i = 0; i < accessory.length; i++) {
+    //   return 
+    // }
+    // const accessoryMap = [accessory].map(img => console.log(img.length && img[img]))
+  
     return (
       <div>
-      <p>These are the accessories</p>
-      {/* <img src={accessory} alt={this.props.state.accessories.name} /> // whatever dot notation returns the images */}
+        <p>These are the accessories</p>
+        <img 
+        src={this.props.accessories.length && this.props.accessories[0].images} 
+        alt={this.props.accessories.length && this.props.accessories[0].name} 
+        />
+        <img 
+        src={this.props.accessories.length && this.props.accessories[1].images} 
+        alt={this.props.accessories.length && this.props.accessories[1].name} 
+        />
+        <img 
+        src={this.props.accessories.length && this.props.accessories[2].images} 
+        alt={this.props.accessories.length && this.props.accessories[2].name} 
+        />
       </div>
     )
   }
